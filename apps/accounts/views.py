786d6fcdbed2267f5b3867faf2e5736941ac8344
SAEED JEDDI <saeedjeddidev@gmail.com>
2025-07-02 05:34:14 +0000

    update 1

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render
-from requests import Response
-from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
-
 class UseIfo(APIView):
-    permission_classes = [IsAuthenticated ]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         return Response({
-            'name': 'request.user.username'
+            'name': request.user.username,
+            'phone': request.user.phone
         })
-
